ab5e5b60b2fe849fc411e5a35b0a6ef0b79844c8
Pepijn de Vos <pepijndevos@gmail.com>
2025-01-21 12:13:39 +0100

    add python

--- ramalama/run.sh
+++ ramalama/run.sh
@@ -4,8 +4,13 @@ REPO=$(jq --raw-output '.repository // empty' $CONFIG_PATH)
 FILE=$(jq --raw-output '.file // empty' $CONFIG_PATH)
 TEMPLATE=$(jq --raw-output '.chat_template // empty' $CONFIG_PATH)
 
-python /get_hf_chat_template.py $TEMPLATE > template.json
-
-llama-server --jinja -hfr "$REPO" -hff "$FILE" -ngl 999 \
-             --chat-template-file template.json \
-             --host 0.0.0.0 --port 10202
+if [ ! -z "$TEMPLATE" ]; then
+  python /get_hf_chat_template.py $TEMPLATE > template.json
+  cat template.json
+  llama-server --jinja -hfr "$REPO" -hff "$FILE" -ngl 999 \
+               --chat-template-file template.json \
+               --host 0.0.0.0 --port 10202
+else
+  llama-server --jinja -hfr "$REPO" -hff "$FILE" -ngl 999 \
+               --host 0.0.0.0 --port 10202
+fi
