2ab66ea6ce41a69757a466603ba9e385cc979d01
Pepijn de Vos <pepijndevos@gmail.com>
2025-01-27 20:21:19 +0100

    partially fixed llama.cpp alpine image

--- llama.cpp/run.sh
+++ llama.cpp/run.sh
@@ -1,6 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-contenv bashio
 
-REPO=$(jq --raw-output '.repository // empty' $CONFIG_PATH)
-FILE=$(jq --raw-output '.file // empty' $CONFIG_PATH)
+export LLAMA_CACHE="/data/models"
+export CONFIG_PATH=/data/options.json
+export GGML_VK_FORCE_MAX_ALLOCATION_SIZE=1073741824
 
-llama-server --jinja -hfr "$REPO" -hff "$FILE" -ngl 999 --host 0.0.0.0 --port 10202
+REPO="$(bashio::config 'repository')"
+FILE="$(bashio::config 'file')"
+TEMPLATE="$(bashio::config 'chat_template')"
+
+if [ ! -z "$TEMPLATE" ]; then
+  python /get_hf_chat_template.py $TEMPLATE > template.json
+  cat template.json
+  llama-server --jinja -hfr "$REPO" -hff "$FILE" -ngl 999 \
+               --chat-template-file template.json \
+               --host 0.0.0.0 --port 10202
+else
+  llama-server --jinja -hfr "$REPO" -hff "$FILE" -ngl 999 \
+               --host 0.0.0.0 --port 10202
+fi
